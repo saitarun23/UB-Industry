09dbfd4e7e5f3e79b1c65da58e8ec2475b6ef440
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2026-01-20 21:40:26 +0530

    done

---
+++ frontend/src/assets/images/index.js
@@ -0,0 +1,17 @@
+import logo from './logo1.jpeg';
+import hero1 from './hero1.png';
+import hero2 from './hero2.png';
+import printing from './printing.png';
+import lamination from './lamination.png';
+import sliting from './sliting.png';
+import pouching from './pouching.png';
+
+export default {
+  logo,
+  hero1,
+  hero2,
+  printing,
+  lamination,
+  sliting,
+  pouching
+}
